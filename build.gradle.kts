0ea3b29e3e1acafd553bf4db1bc51bc60c307d2b
smntin <smn.pankv@gmail.com>
2020-11-30 18:07:17 +0500

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+    id("com.github.johnrengelman.shadow")
+    id("com.dorongold.task-tree") version "1.5"
+}
+
+group = "ru.emkn"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    val junitVersion = "5.6.2"
+
+    implementation(kotlin("stdlib"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    testImplementation(kotlin("test"))
+}
+
+tasks.withType(KotlinCompile::class.java) {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
+
+tasks.shadowJar {
+    archiveBaseName.set("runnable")
+    archiveClassifier.set("")
+    mergeServiceFiles()
+
+    manifest {
+        attributes["Main-Class"] = "ru.emkn.kotlin.MainKt"
+    }
+}
+
+val runJar by tasks.creating(Exec::class) {
+    dependsOn(tasks.shadowJar)
+    val argvString = project.findProperty("argv") as String? ?: ""
+    val jarFile = tasks.shadowJar.get().outputs.files.singleFile
+    val evalArgs = listOf("java", "-jar", jarFile.absolutePath) + argvString.split(" ")
+    commandLine(*evalArgs.toTypedArray())
+}
---
+++ src/main/kotlin/ru/emkn/kotlin/p2beer/main.kt
@@ -0,0 +1,5 @@
+package ru.emkn.kotlin.p2beer
+
+fun main() {
+
+}
