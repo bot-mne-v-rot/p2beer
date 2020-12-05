b8164e2211936eaef3d6a670ff9989c9d4b81f9c
smntin <smn.pankv@gmail.com>
2020-12-06 00:26:51 +0500

    Implement example stream to test the handshake and closure design

---
+++ src/test/kotlin/p2p/network/ExampleStreamListNode.kt
@@ -0,0 +1,91 @@
+package p2p.network
+
+import java.lang.IllegalStateException
+import kotlin.coroutines.*
+import kotlinx.coroutines.*
+
+import ru.emkn.p2beer.p2p.network.*
+
+private class ExampleStreamListNode : StreamListNode() {
+    override var opened: Boolean = false
+
+    private val handshakeMsg = "HaNdShAkE123".toByteArray()
+    private val handshakeRespMsg = "HaNdShAkE123Resp".toByteArray()
+    private val closureMsg = "ClOsUrE123".toByteArray()
+    private val closureRespMsg = "ClOsUrE123Resp".toByteArray()
+
+    private var handshakeContinuation: Continuation<Unit>? = null
+    private var closureContinuation: Continuation<Unit>? = null
+
+    override suspend fun receive(message: Buffer) {
+        val isHandshake = message.contentEquals(handshakeMsg)
+        val isHandshakeResp = message.contentEquals(handshakeRespMsg)
+        val isClosure = message.contentEquals(closureMsg)
+        val isClosureResp = message.contentEquals(closureRespMsg)
+
+        if (!opened && isHandshake) {
+            opened = true
+            send(handshakeRespMsg)
+        } else if (opened && isClosure) {
+            send(closureRespMsg)
+            opened = false
+        } else if (!opened && isHandshakeResp && handshakeContinuation != null) {
+            handshakeContinuation?.resume(Unit)
+            handshakeContinuation = null
+        } else if (opened && isClosureResp && closureContinuation != null) {
+            closureContinuation?.resume(Unit)
+            closureContinuation = null
+        } else if (!opened || isHandshake || isClosure || isHandshakeResp || isClosureResp) {
+            handshakeContinuation?.resumeWithException(HandshakeFailedException())
+            closureContinuation?.resumeWithException(ClosureFailedException())
+            throw IllegalStateException()
+        } else
+            child?.receive(message)
+    }
+
+    override suspend fun send(message: Buffer) {
+        if (!opened)
+            throw IllegalStateException()
+        else
+            parent?.send(message)
+    }
+
+    override suspend fun performHandshake(): Unit = coroutineScope {
+        if (opened)
+            throw IllegalStateException()
+        else {
+            suspendCoroutine<Unit> {
+                handshakeContinuation = it
+                launch { parent?.send(handshakeMsg) }
+            }
+            opened = true
+
+            child?.performHandshake()
+        }
+    }
+
+    override suspend fun performClosure(): Unit = coroutineScope {
+        if (!opened)
+            throw IllegalStateException()
+        else {
+            child?.performClosure()
+
+            suspendCoroutine<Unit> {
+                closureContinuation = it
+                launch { parent?.send(closureMsg) }
+            }
+
+            opened = false
+        }
+    }
+}
+
+class ExampleStreamListNodeTests() {
+    companion object {
+        @StreamListNodeFactory
+        @TestHandshakeAndClosure
+        @JvmStatic
+        fun factoryForExample() : StreamListNode =
+            ExampleStreamListNode()
+    }
+}
