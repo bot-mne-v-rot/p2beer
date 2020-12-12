b259e667fff1549dce3549e0f6f10ca80d5fc531
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2020-12-12 13:58:41 +0300

    Do two windows and fix login. Now it saves information to file

--- src/main/kotlin/ru/emkn/p2beer/app/ui/DialogWindow.kt
+++ src/main/kotlin/ru/emkn/p2beer/app/ui/DialogWindow.kt
@@ -1,47 +1,71 @@
 package ru.emkn.p2beer.app.ui
 
-import com.googlecode.lanterna.TextColor
+import com.googlecode.lanterna.TerminalSize
 import com.googlecode.lanterna.gui2.*
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory
-import java.awt.SystemColor
-import java.io.IOException
+import ru.emkn.p2beer.app.client.chat.ChatImpl
 
+class DialogGUI (private val info : ChatImpl) {
+    fun addDialogWindow(
+        actionListBox : ActionListBox,
+        textGUI : WindowBasedTextGUI,
+    ) {
 
-fun main(args: Array<String>) {
-    try {
+        actionListBox.addItem(info.toString()) {
+            val window: Window = BasicWindow("Dialog Window")
+            window.setHints(listOf(Window.Hint.EXPANDED))
 
-        val screen = DefaultTerminalFactory().createScreen()
-        screen.startScreen()
+            val mainPanel = Panel()
+            mainPanel.layoutManager = BorderLayout()
 
-        val textGUI: WindowBasedTextGUI = MultiWindowTextGUI(screen, TextColor.ANSI.CYAN)
+            val chatPanel = Panel()
 
-        /**
-         * Creating a new window
-         */
-        val window: Window = BasicWindow("Dialog Window")
-        window.setHints(listOf(Window.Hint.FULL_SCREEN));
+            /**
+             * Time for the second big part of mainPanel
+             * It's called chatPanel
+             * It's Location is set to center and that's
+             * how it takes the whole space of the screen,
+             * that is not yet taken by any other elements
+             */
 
-        val mainPanel = Panel()
-        mainPanel.layoutManager = LinearLayout(Direction.HORIZONTAL)
+            chatPanel.layoutManager = BorderLayout()
 
-        val leftPanel = Panel()
-
-        mainPanel.addComponent(leftPanel.withBorder(Borders.singleLine("Left Panel")))
-
-        val rightPanel = Panel()
-        mainPanel.addComponent(rightPanel.withBorder(Borders.singleLine("Right Panel")))
-
-        mainPanel.addComponent(
-            Button("Close", window::close).setLayoutData(
-                GridLayout.createHorizontallyEndAlignedLayoutData(2)
+            mainPanel.addComponent(
+                chatPanel.withBorder(Borders.doubleLineBevel(info.toString()))
+                    .setLayoutData(BorderLayout.Location.CENTER)
             )
-        )
 
-        window.component = mainPanel.withBorder(Borders.singleLine("Main Panel"))
+            /**
+             * In ChatPanel we need to have a textBox to type th message
+             * Its size is defined by the space given to ChatPanel
+             * Its stretched out to the whole width of the panel
+             */
 
-        textGUI.addWindowAndWait(window)
+            val messageField = TextBox(
+                TerminalSize(20, 3),
+                "Type message..."
+            ).setLayoutData(
+                LinearLayout.createLayoutData(LinearLayout.Alignment.Fill)
+            )
 
-    } catch (e: IOException) {
-        e.printStackTrace()
+            /**
+             * messageField is lowered to the bottom
+             * of the chatPanel by the function below
+             */
+
+            chatPanel.addComponent(
+                messageField.setLayoutData(BorderLayout.Location.BOTTOM)
+            )
+
+            mainPanel.addComponent(
+                Button("Close", window::close).setLayoutData(
+                    GridLayout.createHorizontallyEndAlignedLayoutData(2)
+                ).setLayoutData(BorderLayout.Location.BOTTOM)
+            )
+
+            window.component = mainPanel.withBorder(Borders.singleLine("Main Panel"))
+            textGUI.addWindowAndWait(window)
+
+        }
     }
+
 }
---
+++ src/main/kotlin/ru/emkn/p2beer/app/ui/MainWindow.kt
@@ -0,0 +1,103 @@
+package ru.emkn.p2beer.app.ui
+
+import com.googlecode.lanterna.TerminalSize
+import com.googlecode.lanterna.TextColor
+import com.googlecode.lanterna.gui2.*
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory
+import ru.emkn.p2beer.app.client.chat.ChatImpl
+import java.io.IOException
+
+
+fun main(args: Array<String>) {
+    try {
+        /**
+         * Default screen creating
+         */
+        val screen = DefaultTerminalFactory().createScreen()
+        screen.startScreen()
+
+        val textGUI: WindowBasedTextGUI = MultiWindowTextGUI(screen, TextColor.ANSI.CYAN)
+
+        val window: Window = BasicWindow("Dialog Window")
+        //window.setHints(listOf(Window.Hint.EXPANDED));
+
+        /**
+         * Creating a Panel with a BorderLayout
+         * mainPanel consists of three sub-elements which are resizable
+         * due to being non pre-sized
+         */
+
+        val mainPanel = Panel()
+        mainPanel.layoutManager = BorderLayout()
+
+        /**
+         * The first sub-element of the mane Panel is a chatList panel
+         * It uses a Linear Layout so that the list of dialogs with friends
+         * is easy to customize and operate with
+         */
+
+        val chatListPanel = Panel()
+
+        /**
+         * Here LinearLayout.Alignment is set to 'Fill" cause the list
+         * of dialogs must have the same height as the window
+         */
+
+        chatListPanel.layoutData = LinearLayout.createLayoutData(LinearLayout.Alignment.Fill);
+
+        /**
+         * The main component of chatListPanel is, as
+         * was already told - dialogs
+         * They are presented as items in an ActionListBox
+         * @see ActionListBox
+         * Each item is provided with an action triggered when the
+         * item is chosen
+         * The width of [ActionListBox] is set to 20 by default
+         * cause it allows to structure the other parts of the window
+         * and align them around it
+         */
+
+        val actionListBox = ActionListBox(TerminalSize(20, 10))
+        val chat = DialogGUI(ChatImpl("KDizzled"))
+        val chat1 = DialogGUI(ChatImpl("Mimimaxik"))
+        chat.addDialogWindow(actionListBox, textGUI)
+        chat1.addDialogWindow(actionListBox, textGUI)
+        chatListPanel.addComponent(actionListBox)
+
+        /**
+         * Here we locate our freshly created chatListPanel
+         * on the BorderLayout of mainPanel
+         * Now it will be sticked to the left side of the screen
+         */
+
+        mainPanel.addComponent(
+            chatListPanel.withBorder(Borders.doubleLineBevel("Chat list"))
+                .setLayoutData(BorderLayout.Location.LEFT)
+        )
+
+        /**
+         * Last, but not least, a tiny 'exit' button
+         * is located at the bottom of the BorderLayout
+         * This way it goes right below both panels
+         * with messages and chats
+         */
+
+        mainPanel.addComponent(
+            Button("Close", window::close).setLayoutData(
+                GridLayout.createHorizontallyEndAlignedLayoutData(2)
+            ).setLayoutData(BorderLayout.Location.BOTTOM)
+        )
+
+        /**
+         * Make the window active
+         */
+
+        window.component = mainPanel.withBorder(Borders.singleLine("Main Panel"))
+        textGUI.addWindowAndWait(window)
+
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+}
+
+
